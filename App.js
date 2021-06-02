1b2fda2e2f15742a4426f5c77137c105b731bab1
Shoman-28 <northstar9928@gmail.com>
2021-06-03 03:56:58 +0600

    dd

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import image from "./1622668996048.jpg"
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Know Your Country</Text>
+      <Image
+          source={image}
+          style={{ width: 200, height: 200 }}
+        />
       <StatusBar style="auto" />
     </View>
   );
